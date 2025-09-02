11ea9ceae50296229d410cb0c3de74efa2a9a1de
golitter <golitter@qq.com>
2025-09-02 11:27:12 +0800

    chatbot 提示词调整到yaml文件内

--- src/chat/chatbot.py
+++ src/chat/chatbot.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 from langchain.chat_models import init_chat_model
 from src.models.lazy_llm import LazyRerankLLM
 from langchain_core.prompts import ChatPromptTemplate
+from src.config import load_prompt_templates
+custom_prompt_templates = load_prompt_templates()
 
 # 加载环境变量
 load_dotenv()
@@ -29,13 +31,8 @@ def chat_with_llm(prompt):
 # print(f"Answer: {answer}")
 
 
-system_template = """
-你叫陕珂娜（Zhibot），是一个专注于陕西科技大学信息的智能助手。你以轻小说《魔女之旅》的主角——旅途中的魔女伊雷娜的口吻来回答问题。
+system_template = custom_prompt_templates.get('zhibo_shannona_prompt', {}).get('naive_system_prompt')
 
-你的语气轻松、优雅，略带一点俏皮与自信，却始终保持理性与智慧。就像一位陪伴旅人走过城市街巷的向导，你善于将复杂的现实问题，用清晰又富有韵味的语言娓娓道来。
-
-{{不能使用虚构世界的魔法或幻想设定}}，而是专注于现实社会中的问题解答，例如高校政策、奖助学金、课程设置、就业信息、校园生活、社会实践、科研方向等。
-"""
 
 
 prompt_template = ChatPromptTemplate.from_messages(
